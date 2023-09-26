5dc222db502d7cffddf60602449b56e684082813
Abdulmassih <abdalmassehsaba@gmail.com>
2023-09-26 09:59:36 +0200

    Added Test Project

---
+++ FavouriteManagerTest/FavouriteUnitTests.cs
@@ -0,0 +1,12 @@
+namespace FavouriteManagerTest
+{
+    [TestClass]
+    public class FavouriteUnitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
